1c7a56cdc64c74fbef22714886ada3662e58366a
Ad-gangwar <adgangwar54@gmail.com>
2023-12-23 01:00:05 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ require('dotenv').config();
 const PORT=process.env.PORT || "5000";
 const fetchData=require('./db')
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use((req, res, next)=>{
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
@@ -15,11 +17,15 @@ app.use((req, res, next)=>{
 })
 // Middleware to parse JSON requests
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "./backend/build")));
 app.get("/", (req, res) => {
     res.send("hello");
 });
-
+app.get('*', function (_, res){
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"), function(err){
+        res.status(500).send(err);
+    })
+})
 // Assuming 'createUser' is a router module
 app.use('/api', require('./Routes/createUser'));
 app.use('/api', require('./Routes/submitSecret'));
